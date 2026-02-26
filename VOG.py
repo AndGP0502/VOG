de0b9588d4e7eeada9d1ee3accc3c5844f096722
AndGP0502 <andre.garzon@udla.edu.ec>
2026-02-26 15:37:17 -0500

    Actualizado

--- VOG.py
+++ VOG.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/sobre")
-def sobre():
-    return render_template("sobre.html")
+@app.route("/galeria")
+def galeria():
+    return render_template("galeria.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
